ed531e30c30a612d75069ede9fda350036339352
Bhamid4 <113592135+Bhamid4@users.noreply.github.com>
2023-06-24 13:55:53 -0400

    Added add,edit and delete buttons to index.ejs

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI":  "mongodb+srv://BilalHamid:B123456789l@mongodbserver.opav6jv.mongodb.net/book_store?retryWrites=true&w=majority"
 };
